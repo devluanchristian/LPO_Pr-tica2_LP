4751a035c5e0216ed07d4f5088b235fb2c2e4240
Luan Christian <90977582+devluanchristian@users.noreply.github.com>
2022-09-27 21:15:59 -0300

    Update Figurinha.java
    Codigo atualizado, foi adicionado JOptionPane para exibição de janelas interativas com o usuario

--- LPO_Pratica1/src/br/com/newton/exercicio/figurinha/Figurinha.java
+++ LPO_Pratica1/src/br/com/newton/exercicio/figurinha/Figurinha.java
@@ -2,44 +2,39 @@ package br.com.newton.exercicio.figurinha;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class Figurinha {
 
-	public static void main(String[] args) {
-		String[]nomes = new String[5];
-		String[]selecao = new String[5];
-		int[]idade = new int[5];
-		int media = 0;
-		
-		
-		for(int i = 0; i < 4; i++) {
-			
-			System.out.println("Informe o nome de uma selecao: ");
-			selecao[i] = new Scanner(System.in).nextLine();
-			
-			System.out.println("Informe o nome de algum jogador da(o) "+selecao[i]);
-			nomes[i] = new Scanner(System.in).nextLine();
-			
-			System.out.println("Qual a idade do jogador "+nomes[i]+ " da selecao da(o) "+selecao[i]);
-			idade[i] = new Scanner(System.in).nextInt();
-			
-			switch(nomes[i]) {
-			case "Marquinhos":
-				System.out.println("Marquinhos vai levantar a taça do Hexa. ");
-				break;
-			case "Hulk":
-				System.out.println("Dessa vez não vai dar para você! . ");
-				break;
-			}
-			switch(selecao[i]) {
-			case "Argentina":
-				System.out.println("Adeus los hermanos!!!. ");
-				break;
-			
-		}
-			media = idade[i]/5;
-			System.out.println(media);
+    public static void main(String args[]) {
+        String[] nome = new String[5];
+        String[] selecao = new String[5];
+        int[] idade = new int[5];
+        int sumIdade = 0, mediaIdade = 0;
 
-	}
-		
-  }
+        Scanner ler = new Scanner(System.in);
+        for (int i = 0; i < nome.length; i++) {
+            nome[i] = JOptionPane.showInputDialog("Entre com o nome do jogador da figurinha");
+            selecao[i] = JOptionPane.showInputDialog("Entre com a selecao deste jogador");
+            idade[i] = Integer.parseInt(JOptionPane.showInputDialog("Entre com a idade do jogador"));
+
+        }
+        for (int i = 0; i < idade.length; i++) {
+            if (nome[i].equals("Marquinhos")) {
+                JOptionPane.showMessageDialog(null, "Marquinhos vai levantar a taça", "Mensagem", 1);
+            } else if (nome[i].equals("Hulk")) {
+                JOptionPane.showMessageDialog(null, "Dessa vez não vai dar pra você", "Hulk", 2);
+
+            }
+            if (selecao[i].equals("Argentina")) {
+                JOptionPane.showMessageDialog(null, "Adeus los hermanos", "Argentina", 2);
+
+            }
+            sumIdade = sumIdade + idade[i];
+        }
+        mediaIdade = (sumIdade / idade.length);
+        JOptionPane.showMessageDialog(null, "A copa do mundo possui uma media de idade " + mediaIdade,
+                "Media da Copa do Mundo", 1);
+
+    }
 }
