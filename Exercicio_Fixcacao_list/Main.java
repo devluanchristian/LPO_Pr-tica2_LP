7464ec66e5752f6d5596292af3591a8ea8d2fe90
Luan Christian <90977582+devluanchristian@users.noreply.github.com>
2022-10-19 20:09:56 -0300

    Concluido

--- Exercicio_Fixcacao_list/Main.java
+++ Exercicio_Fixcacao_list/Main.java
@@ -1,27 +1,36 @@
-
 public class Main {
     public static void main(String[] args) {
 
-        Pizza p = new Pizza();
+        Pizza p = new Pizza("Calabresa", 0, "P", false);
+        Pizza p2 = new Pizza("Calabresa", 0, "M", false);
+
+        Pedido pdd = new Pedido(null, "12/12/12");
 
         Ingrediente i = new Ingrediente("Mussarela");
-        Ingrediente i2 = new Ingrediente("Bacon");
+        Ingrediente i2 = new Ingrediente("Banco");
+        Ingrediente i3 = new Ingrediente("Banco");
+        Ingrediente i4 = new Ingrediente("Banco");
+        Ingrediente i5 = new Ingrediente("Banco");
 
-        //p.listaIngredientes.add(i);
-        
-        //p.listaIngredientes.add(i2);
+        // metodo que adicionando ingrediente na pizza
+        p.adcIngrediente(i);
+        p.adcIngrediente(i2);
+        p.adcIngrediente(i3);
+        p.adcIngrediente(i4);
+        p.adcIngrediente(i5);
 
-        //p.adicionarIngredientesPizza(i);
-       // p.adicionarIngredientesPizza(i2);
+        // metodo que adiciona pizza no pedido
+        pdd.adcPizza(p);
+        pdd.adcPizza(p2);
 
-       p.calculaPrecoPizza("G");
+        // imprimir a quantidade de ingrediente que foi adicionado
+        System.out.println("Quantidades de ingredientes adicionados: "+p.getQuantidadeIngrediente());
 
+        // imprimir o valor calculado da pizza
+        System.out.println("Preco calculado da pizza apos adicionar os ingredientes: "+p.calculaPizza());
 
-
-
-        System.out.println(p.getQuantidadeIngredientes());
+        System.out.println(pdd.imprimeResumoPedido());
 
     }
-        
 
 }
