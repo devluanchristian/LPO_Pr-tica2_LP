fbc9149f9901c1312c95b400a0c5eb51256acefb
Luan Christian <90977582+devluanchristian@users.noreply.github.com>
2022-10-18 22:06:11 -0300

    Update Pedido.java

--- Exercicio_Fixcacao_list/Pedido.java
+++ Exercicio_Fixcacao_list/Pedido.java
@@ -1,7 +1,14 @@
-public class Pedido extends Cliente {
+public class Pedido {
     private int numeroPedido;
     private String data;
-    
 
-    
+    Cliente cliente;
+    Pizza p = new Pizza();
+
+    public Pedido(int numeroPedido, String data, Cliente cliente) {
+        this.numeroPedido = numeroPedido;
+        this.data = data;
+
+    }
+
 }
