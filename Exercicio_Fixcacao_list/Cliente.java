16e24e99135d3e8c427aa2224695f641e630b2fa
Luan Christian <90977582+devluanchristian@users.noreply.github.com>
2022-10-19 20:09:17 -0300

    Concluido

--- Exercicio_Fixcacao_list/Cliente.java
+++ Exercicio_Fixcacao_list/Cliente.java
@@ -2,32 +2,27 @@ public class Cliente {
     private String nome;
     private String telefone;
 
-    
     public Cliente(String nome, String telefone) {
         this.nome = nome;
         this.telefone = telefone;
     }
 
+    public Cliente(String[] args) {
+    }
 
     public String getNome() {
         return nome;
     }
 
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
 
     public String getTelefone() {
         return telefone;
     }
 
-
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
-
-    
-    
 }
