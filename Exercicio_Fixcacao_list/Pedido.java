637e5cff4f7ef64f3aed37f27ec219918094d92e
Luan Christian <90977582+devluanchristian@users.noreply.github.com>
2022-10-18 22:12:49 -0300

    Update Pedido.java

--- Exercicio_Fixcacao_list/Pedido.java
+++ Exercicio_Fixcacao_list/Pedido.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedido {
     private int numeroPedido;
     private String data;
-
     Cliente cliente;
-    Pizza p = new Pizza();
+    List<Pizza> listaPizza = new ArrayList<Pizza>();
+    
 
     public Pedido(int numeroPedido, String data, Cliente cliente) {
         this.numeroPedido = numeroPedido;
