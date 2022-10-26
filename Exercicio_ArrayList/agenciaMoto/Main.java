2e8416c520b49b1c2c4f14df4e24ec03e0c96e8b
Luan Christian <90977582+devluanchristian@users.noreply.github.com>
2022-10-25 21:30:56 -0300

    Update Main.java

--- Exercicio_ArrayList/agenciaMoto/Main.java
+++ Exercicio_ArrayList/agenciaMoto/Main.java
@@ -6,15 +6,19 @@ public class Main {
         Moto moto = new Moto();
         moto.nome = "CG";
         moto.cor = "Vermelha";
+        moto.valor = 22.500;
 
         Moto moto2 = new Moto();
         moto2.nome = "Tyger";
         moto2.cor = "Cinza";
+        moto2.valor = 12.500;
 
         Agencia ag = new Agencia();
         ag.adicionarCarro(moto);
         ag.adicionarCarro(moto2);
 
         System.out.println(ag.getQuantidadeMoto());
+        System.out.println("Valor total das motos : "+ag.calculaValor());
+
     }
 }
