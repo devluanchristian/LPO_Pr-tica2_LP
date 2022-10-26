39a032cb615466a2971bfda527a3c81cd0d696bf
Luan Christian <90977582+devluanchristian@users.noreply.github.com>
2022-10-25 21:30:16 -0300

    Update Main.java

--- Exercicio_ArrayList/agencia/Main.java
+++ Exercicio_ArrayList/agencia/Main.java
@@ -5,16 +5,18 @@ public class Main {
         Carro carro = new Carro();
         carro.nome = "Palio";
         carro.cor = "Cinza";
+        carro.valor = 50.000;
 
         Carro carro2 = new Carro();
         carro2.nome = "Gol";
         carro2.cor = "Preta";
+        carro2.valor = 40.000;
 
         Agencia ag = new Agencia();
         ag.nome = "Agencia do Luanzin";
         ag.adicionarCarro(carro);
         ag.adicionarCarro(carro2);
-        System.out.println("Na "+ag.nome+" tem "+ag.getQuantidadeCarros()+" carros");
+        System.out.println("Na "+ag.nome+" tem "+ag.getQuantidadeCarros()+" carros" + "\nValores total dos carros é: "+ ag.calculaValor());
     }
 
 }
